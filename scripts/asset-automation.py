c47587eeb935ba703f53dd990c4cd3793bea9af2
Xavier Xu <xavierxu1@gmail.com>
2026-06-17 17:57:20 +0700

    Updated env to include API key

--- scripts/asset-automation.py
+++ scripts/asset-automation.py
@@ -6,9 +6,14 @@ Step 2:
     Read CSV file with asset information
 
 Step 3:
+    Check if asset exists in snipe-it
+
+Step 4:
     Check if model exists in snipe-it
-    If not, create model and add to snipe it
-Step 4:Then create the asset and add to snipe it
+    If not, create model and add to snipe-it
+
+Step 5:
+    Create the asset and add to snipe-it
 """
 
 import os, csv, requests
@@ -16,6 +21,15 @@ from dotenv import load_dotenv
 
 load_dotenv() # Load environment variables from .env file
 
-token = os.getenv("APP_KEY")
+token = os.getenv("API_KEY")
+print(token)
 
+'''
+with open("../data/Dummy data_new.csv", 'r') as datafile:
+    csvreader = csv.reader(datafile)
+    header = next(csvreader) # Skip header row
+    
+    for row in csvreader:
+        print(row)
+'''
 
