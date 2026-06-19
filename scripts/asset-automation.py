d3acea138222f818deacd0d83b16b3574fa8e7e0
Xavier Xu <xavierxu1@gmail.com>
2026-06-19 16:04:03 +0700

    Script v1 and removed name column from data

--- scripts/asset-automation.py
+++ scripts/asset-automation.py
@@ -32,24 +32,42 @@ r1 = requests.get(
             "Content-Type": "application/json",
             "Accept": "application/json"
             }
-    )
+)
 models = r1.json()
 r2 = requests.get(
     "http://localhost:8000/api/v1/manufacturers",
     headers = {"Authorization": f"Bearer {token}",
             "Content-Type": "application/json",
             "Accept": "application/json"
-            }            )
+            }            
+)
 manufacturers = r2.json()
 r3 = requests.get(
     "http://localhost:8000/api/v1/categories",
     headers = {"Authorization": f"Bearer {token}",
             "Content-Type": "application/json",
             "Accept": "application/json"
-            }            )
+            }            
+)
 categories = r3.json()
+r4 = requests.get(
+    "http://localhost:8000/api/v1/locations",
+    headers = {"Authorization": f"Bearer {token}",
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+            }            
+)
+locations = r4.json()
+r5 = requests.get(
+    "http://localhost:8000/api/v1/users",
+    headers = {"Authorization   ": f"Bearer {token}",
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+            }
+)
+users = r5.json()
 
-
+# Open CSV
 with open(dataPath, 'r') as datafile:
     csvreader = csv.reader(datafile)
     header = next(csvreader) # Skip header row
@@ -57,17 +75,13 @@ with open(dataPath, 'r') as datafile:
     for row in csvreader:
         if row[1] not in [model['name'] for model in models['rows']]: # Check if model exists in model data
 
-            # Cross-reference manufacturer and model category ID with asset data
-            row[3] = next((manufacturer['id'] for manufacturer in manufacturers['rows'] if manufacturer['name'] == row[3]), None) # Get manufacturer ID from name in CSV
-            row[2] = next((category['id'] for category in categories['rows'] if category['name'] == row[2]), None) # Get category ID from name in CSV
-
             # Create model in Snipe-IT
             newModelData = {
                 "name": row[1],
-                "manufacturer_id": row[3],
-                "category_id": row[2],
+                "manufacturer_id": next((manufacturer['id'] for manufacturer in manufacturers['rows'] if manufacturer['name'] == row[3]), None), # Get manufacturer ID from name in CSV
+                "category_id": next((category['id'] for category in categories['rows'] if category['name'] == row[2]), None) # Get category ID from name in CSV
             }
-            r4 = requests.post(
+            p = requests.post(
                 "http://localhost:8000/api/v1/models",
                 headers = {"Authorization": f"Bearer {token}",
                         "Content-Type": "application/json",
@@ -75,8 +89,41 @@ with open(dataPath, 'r') as datafile:
                         },
                 data = json.dumps(newModelData)
             )
+            print(f"Created model: {row[1]} with status code {p.status_code}")
+        else:
+            print(f"Model {row[1]} already exists in Snipe-IT, skipping model creation.")
 
-            print(f"Created model: {row[1]} with status code {r4.status_code}")
-        print(row)
+        # Add assets from CSV
+        newAssetData = {
+            "asset_tag": row[0],
+            "model_id": next((model['id'] for model in models['rows'] if model['name'] == row[1]), None), # Get model ID from name in CSV
+            "category_id": next((category['id'] for category in categories['rows'] if category['name'] == row[2]), None), # Get category ID from name in CSV
+            "manufacturer_id": next((manufacturer['id'] for manufacturer in manufacturers['rows'] if manufacturer['name'] == row[3]), None), # Get manufacturer ID from name in CSV
+            "status_id": row[4],
+            "serial": row[5],
+            "_snipeit_mac_address_1": row[6],
+            "_snipeit_cpu_2": row[7],
+            "_snipeit_ram_3": row[8],
+            "_snipeit_storage_4": row[9],
+            "_snipeit_operating_system_5": row[10],
+            "_snipeit_graphics_6": row[11],
+            "purchase_date": row[12],
+            "purchase_cost": row[13],
+            "rtd_location_id": next((location['id'] for location in locations['rows'] if location['name'] == row[14]), None), # Get location ID from name in CSV
+        }
+
+        p2 = requests.post(
+            "http://localhost:8000/api/v1/hardware",
+            headers = {"Authorization": f"Bearer {token}",
+                    "Content-Type": "application/json",
+                    "Accept": "application/json"
+                    },
+            data = json.dumps(newAssetData)
+        )
+
+        print("All assets loaded into Snipe-IT!")
+
+
+            
 
 
